a344c918577113a16dec76ae153b59994fd44cf3
Eric J Gruber <Eric.Gruber@vml.com>
2016-12-02 09:16:30 -0600

    Add base structure for the index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TweetSheet</title>
+
+  </head>
+  <body>
+    <div class="container">
+    <?php
+    require_once('TWitterAPIExchange.php');
+
+
+
+  </div>
+
+  </body>
+</html>
