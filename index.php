2ab28266bd61c8d76a538b7abede97e4afe46eaa
Eric J Gruber <Eric.Gruber@vml.com>
2016-12-02 09:17:29 -0600

    Add Bootstrap CDN link

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>TweetSheet</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
 
   </head>
   <body>
@@ -10,6 +11,7 @@
     <?php
     require_once('TWitterAPIExchange.php');
 
+    ?>
 
 
   </div>
