e3749a68eebff8de0fad10b773492367c0c0ca38
Eric J Gruber <Eric.Gruber@vml.com>
2016-12-02 09:18:32 -0600

    Add Bootstrap JS CDN link

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>TweetSheet</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" media="screen" title="no title">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
 
   </head>
   <body>
