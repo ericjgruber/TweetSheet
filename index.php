e93fa272ca6231f025b0ab69025497e55ac96f97
Eric J Gruber <Eric.Gruber@vml.com>
2016-12-02 09:22:08 -0600

    Set all the request information to get the data from Twitter and parse it

--- index.php
+++ index.php
@@ -25,6 +25,17 @@
 
     $url = "https://api.twitter.com/1.1/statuses/home_timeline.json";
 
+    $requestMethod = "GET";
+    $getfield = '?screen_name=iagdotme&count=50';
+    $twitter = new TwitterAPIExchange($settings);
+    $string = json_decode($twitter->setGetfield($getfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest(),$assoc = TRUE);
+    if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+    // echo "<pre>";
+    // print_r($string);
+    // echo "</pre>";
+
     ?>
 
 
